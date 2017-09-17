dad56023b9d9c1e499735aad8539c5052757f042
Affliction123 <mwmsigma@gmail.com>
2017-09-17 19:05:27 +0200

    Update FDS-HW1-PrimeNumber.py

--- FDS-HW1-PrimeNumber.py
+++ FDS-HW1-PrimeNumber.py
@@ -5,3 +5,13 @@ def prime(n):
 	if n == 2 or n == 3:
 		print("Prime") 
 		sys.exit()
+	elif n < 2:
+		print("Choose bigger number, and run program again")
+		sys.exit()
+	elif n%2 == 0:
+		print("2")
+		sys.exit()	
+	elif n < 9:
+		print("Prime")
+		sys.exit()
+	
