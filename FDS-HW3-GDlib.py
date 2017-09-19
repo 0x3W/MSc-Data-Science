06378c2658ae4b8dfd776559ae99571fc45e824f
Affliction123 <mwmsigma@gmail.com>
2017-09-20 01:36:06 +0200

    Create FDS-HW3-GDlib.py

---
+++ FDS-HW3-GDlib.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def descent(y, x, alpha=1e-3, itr=1e2, eps=1e-6):
+    def convergence(theta, previous_theta):
+        diff_norm = np.linalg.norm(theta - previous_theta, ord=1)
+        new_norm = np.linalg.norm(theta, ord=1)
+
+        if new_norm == 0:
+            return False
+
+        return diff_norm / new_norm <= eps
+
+    # This extends the input observations to include a bias factor 1.
+    X = np.hstack((np.ones((x.shape[0], 1)), x))
+
+    # Initialize the coefficient's vector with ones.
+    theta = np.zeros((x.shape[1] + 1, 1))
+
+    for iteration in range(int(itr)):
+        previous_theta = np.copy(theta)
+
+        # Compute the gradient
+        gradient = -2 * X.T.dot(y - np.dot(X, theta)[:, 0])
+
+        theta -= alpha * gradient[:, np.newaxis] #/ X.shape[0]
+
+        # Check if the convergence criterion is satisfied
+        if convergence(theta, previous_theta):
+            break
+
+    # Return the final thetas
+    return theta.ravel()
+
+
