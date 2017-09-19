9db1552b2a99abb19e1cffce79f444e6736325ee
Affliction123 <mwmsigma@gmail.com>
2017-09-20 01:34:59 +0200

    Update and rename FDS - HW3 - GDrun.py to FDS-HW3-GDrun.py

--- FDS-HW3-GDrun.py
+++ FDS-HW3-GDrun.py
@@ -22,3 +22,19 @@ print('Coefficients:', theta)
 print('R2:', IDlib.r2(y, theta, XAGST))
 print()
 
+plt.scatter(XAGST, y)
+
+indexPred = np.argsort(X[:, header.index('AGST')])
+predictions = IDlib.predict(XAGST, theta)
+plt.plot(XAGST[indexPred, 0], predictions[indexPred],
+         color='red', linewidth=3)
+plt.savefig('1772953.png')
+
+features = ['AGST', 'WinterRain', 'HarvestRain', 'Age']
+Xmulti = X[:, [header.index(f) for f in features]]
+theta = IDlib.descent(y, Xmulti, alpha=1e-2, itr=1e6, eps=1e-6)
+
+print('Features: AGST, WinterRain, HarvestRain, Age')
+print('Coefficients:', theta)
+print('R2:', IDlib.r2(y, theta, Xmulti))
+print()
