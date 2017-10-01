09850d10ad3f213298dc97fbb72f1673cb377aec
Affliction123 <mwmsigma@gmail.com>
2017-10-01 12:54:13 +0200

    Update FDS-HW4-lib.py

--- FDS-HW4-lib.py
+++ FDS-HW4-lib.py
@@ -26,3 +26,82 @@ def wordcount(filename):
             count=wordsBag[word]
             wordsBag[word]=count+1
     return wordsBag
+
+def bag(wc, treshold=10):
+    d1 = [k for k, v in wc.items() if v > treshold] 
+    return d1
+    
+def jaccard(s1,s2):
+    sUnion = s1.union(s2)
+    sInter = s1.intersection(s2)
+    
+    lenUnion = len(sUnion)
+    if not lenUnion:
+        return 0
+    
+    return len(sInter)*1.0/lenUnion
+
+def single_linkage(D, k):
+    count = len(D) - k
+    i = 1
+    D[D == 0] = 1
+    unRavels=[]
+    while (i <= k):
+        #print ("The count is:", i)
+
+    # Get everything for rows
+    # Unravel rows
+        temp1 = np.unravel_index(D.argmin(), D.shape)
+        #print(temp1)
+
+    # Unravel columns
+        temp2 = temp1[::-1]
+
+    # Select min rows, and find min of them
+        #print("Minimum rows")
+        finMat2 = D[np.ix_(temp1)]                
+        tempMatR = np.amin(finMat2, axis=0)
+        #print(tempMatR)
+
+    # Select min columns, and find min of them
+        #print("Minimum columns")
+        finMat3 = D[np.ix_(temp2)]
+        #print(finMat3.T)
+        tempMatC = np.amin(finMat3, axis=0)
+    #tempMatC1 = np.transpose(tempMatC)
+        #print(tempMatC)
+
+    #Add character due to larger rows
+        #print("TryOut")
+        addOne = np.array([1])
+        #print(addOne)
+        addOneTo = np.hstack((tempMatC,addOne))
+        #print(addOneTo)
+        addOneTo1 = addOneTo.reshape((-1, 1))
+        #print(addOneTo1)
+
+        #print("stacked mins for rows")
+        tempMat2 = np.vstack((D,tempMatR))
+        #print(tempMat2)
+
+    # Try to combine on new matrix
+        #print("stacked mins for cols")
+        combLarge = np.hstack((tempMat2,addOneTo1))
+        #print(combLarge)
+
+    #Deleting
+        #print(temp1)
+        #print("Deleted excess from first iter, row")
+        tempMat3 = np.delete(combLarge, np.ix_(temp1), axis=0)
+        #print(tempMat3)
+
+        #print(temp2)
+        #print("Deleted excess from first iter, column")
+        tempMat4 = np.delete(tempMat3, np.ix_(temp2), axis=1)
+        #print(tempMat4)
+        unRavels.append(temp1)
+        D = tempMat4
+        #print(D)
+        i = i + 1
+    return D, unRavels
+
