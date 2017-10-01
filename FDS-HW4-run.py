e45e141aa50da377adb91369c754df5f049178c0
Affliction123 <mwmsigma@gmail.com>
2017-10-01 12:49:41 +0200

    Create FDS-HW4-run.py

---
+++ FDS-HW4-run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  9 17:29:54 2016
+
+@author: Dovla
+"""
+
+import sys
+import numpy as np
+import IDlibtest1 as mylib
+
+
+a = len(sys.argv)
+b = str(sys.argv)
+#print(a,b)
+
+bagsLength = []
+jacResults = []
+i = 1
+
