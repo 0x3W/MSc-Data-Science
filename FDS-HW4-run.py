cea544a88e61819dcfd006fdb8a4ee0236d4d23b
Affliction123 <mwmsigma@gmail.com>
2017-10-01 12:51:36 +0200

    Update FDS-HW4-run.py

--- FDS-HW4-run.py
+++ FDS-HW4-run.py
@@ -18,4 +18,24 @@ b = str(sys.argv)
 bagsLength = []
 jacResults = []
 i = 1
+for i in range(a):
+    firstWc = mylib.wordcount(sys.argv[i])
+    firstBag = mylib.bag(firstWc, treshold=200)
+    bagsLength.append(len(firstWc))
+    ii = 1
+    for ii in range(a):
+        secondWc = mylib.wordcount(sys.argv[ii])
+        secondBag = mylib.bag(secondWc, treshold=200)
+        jacResults.append(1-mylib.jaccard(set(firstBag),set(secondBag)))
+        #print(jacResults)	
+        if ii > a:
+            break
+        else:
+            ii = ii + 1
+    
+    if i > a:
+        break
+    else:
+        i = i + 1
+
 
