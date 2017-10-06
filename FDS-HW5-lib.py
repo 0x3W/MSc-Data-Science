6b0611073e8430ce192aee07a8082e3d6c1953d8
Affliction123 <mwmsigma@gmail.com>
2017-10-06 12:08:30 +0200

    Create FDS-HW5-lib.py

---
+++ FDS-HW5-lib.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 24 15:58:52 2016
+
+@author: Dovla
+"""
+
+import numpy as np
+def charfreq(filename, filter):
+    dc = {c:0 for c in filter}
+    f = open(filename, "r")
+    for line in f:
+        for ch in line:
+            if ch in dc:
+                dc[ch] +=1
+    f.close()
+    fv = np.zeros(len(filter))
+    for i in range(len(filter)):
+        fv[i] = dc[filter[i]]
+    return fv/fv.sum() if fv.sum() > 0 else fv
+
