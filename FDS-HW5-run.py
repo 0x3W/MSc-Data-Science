164b0199a5d0f4cc567fd18592b885db1ff16916
Affliction123 <mwmsigma@gmail.com>
2017-10-06 12:07:31 +0200

    Create FDS-HW5-run.py

---
+++ FDS-HW5-run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 24 15:41:56 2016
+
+@author: Dovla
+"""
+
+import sys
+import lib1772953 as lib
+
+files = sys.argv[1:-2]
+filter = sys.argv[-2]
+nrClst = int(sys.argv[-1])
