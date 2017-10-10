17671eb34fd4e027a95a28395e3478c2975697aa
W <mwmsigma@gmail.com>
2017-10-10 18:03:59 +0200

    Create FDS-HW7-run.py

---
+++ FDS-HW7-run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 12 17:41:33 2016
+
+@author: Dovla
+"""
+import sys
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import lib1772953 as lib
+
