0b115083b032b53e1b3d60dca94f2ef8150f3531
W <mwmsigma@gmail.com>
2017-10-10 18:04:15 +0200

    Update FDS-HW7-run.py

--- FDS-HW7-run.py
+++ FDS-HW7-run.py
@@ -12,3 +12,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import lib1772953 as lib
 
+data = pd.read_csv(sys.argv[1])
+args = sys.argv[2:]
+
+datap = pd.DataFrame.pivot(data, index=data.columns[0], 
+                           columns=data.columns[1], values=data.columns[2])
+datap.fillna(datap.mean(axis=0), axis=0, inplace=True)
+
+r = random.sample(range(0,datap.shape[0]), 250)
+
+
+plt.ion()
+plt.grid()    
+for i in args:
+    lib.first(r,datap,int(i))
+plt.show(block=True)
