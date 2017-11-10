7fc9db381a97394d76220f49033528b8b797ee4c
W <mwmsigma@gmail.com>
2017-11-11 00:22:47 +0100

    Update DMT-hw3Part1.py

--- DMT-hw3Part1.py
+++ DMT-hw3Part1.py
@@ -54,3 +54,55 @@ print(Y_test.shape)
 print(target_names)
 
 
+
+vectorizer = TfidfVectorizer(strip_accents= None,
+							preprocessor = None,
+							)
+#nbc = MultinomialNB()
+knnC = KNeighborsClassifier()
+
+pipeline = Pipeline([
+	('vect', vectorizer),
+	('knnC', knnC),
+	])
+
+parameters = {
+	'vect__tokenizer': [None, stemming_tokenizer],
+	'vect__ngram_range': [(1, 1), (1, 2),],
+	#'nbc__alpha': [.001,.01, 1.0, 10.],
+    'knnC__n_neighbors':[3,5,10],
+    'knnC__leaf_size':[30,50],
+	}
+
+grid_search = GridSearchCV(pipeline,
+						   parameters,
+						   scoring=metrics.make_scorer(metrics.matthews_corrcoef),
+						   cv=10,
+						   n_jobs=2,
+						   verbose=10)
+
+grid_search.fit(X_train, Y_train)
+
+
+Y_predicted = grid_search.predict(X_test)
+
+# Evaluate the performance of the classifier on the original Test-Set
+#####################################################3
+output_classification_report = metrics.classification_report(
+									Y_test,
+									Y_predicted,
+									target_names=target_names)
+print(output_classification_report)
+
+# Compute the confusion matrix
+confusion_matrix = metrics.confusion_matrix(Y_test, Y_predicted)
+print("Confusion Matrix: True-Classes X Predicted-Classes")
+print(confusion_matrix)
+
+print("Matthews Corr Coef")
+pp.pprint(metrics.matthews_corrcoef(Y_test, Y_predicted))
+
+pp.pprint(grid_search.best_params_)
+
+print("Accuracy")
+pp.pprint(metrics.accuracy_score(Y_test, Y_predicted))
